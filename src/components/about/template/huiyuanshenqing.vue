8ae4f3fbe0452beb437a4f5e145cf30d1652c4f8
bugall <769088641@qq.com>
2018-10-23 15:13:46 +0800

    static page finish

---
+++ src/components/about/template/huiyuanshenqing.vue
@@ -0,0 +1,100 @@
+<template>
+<ul class="f-l listab-13"><!-- Membership Application -->
+                <div class="c-box">
+                	<div class="r-box animat-l">
+                		<div class="animation-box">
+                			<img src="static/images/animation3-1.png"  class="f-l     animation1-img" />
+                			<img src="static/images/animation3-2.png"  class="f-l img animation2-img" />
+                			<img src="static/images/animation3-3.png"  class="f-l img animation3-img" />
+                			<img src="static/images/animation3-4.png"  class="f-l img animation4-img" />
+                			<img src="static/images/animation3-5.png"  class="f-l img animation5-img" />
+                			<img src="static/images/animation3-6.png"  class="f-l img animation6-img" />
+                			<img src="static/images/animation3-7.png"  class="f-l img animation7-img" />
+                			<img src="static/images/animation3-8.png"  class="f-l img animation8-img" />
+                			<img src="static/images/animation3-9.png"  class="f-l img animation9-img" />
+                		</div>
+                	</div>  	
+                	<div class="l-box">
+                		<h2 class="f-l c-navy"> 群成员应用   </h2>
+                		<h3 class="f-l c-navy"> 会员享有下列权利：  </h3>
+                		<div class="c-box">
+                		  <h5 class="f-l c-gray">
+                        （一）本研究所的选举权、表决权和表决权。<br/>              
+                        （二）参加研究所的活动；              <br/>   
+                        （三）获得研究所服务的优先权；            <br/>     
+                        （四）对研究所工作的批评和建议权；       <br/>         
+                        （五）会员资格将是自由和自愿的。<br/>   
+                		  </h5>	
+                		  <h4 class="f-l c-navy"> 
+                		  	请自行下载，填写并加盖公章，然后连同单位详细情况和扫描文件一起邮寄至本邮箱：<a href="#" class="c-navy-h c-green"> tuantihuiyuan@idc.com </a>
+                		  </h4>	
+                		</div>	
+                		<a href="#" class="btn-2 bgc-green c-white bgc-blue1-h btn-download"> 申请表下载  </a>
+                	</div>
+                	<div class="r-box animat-r">
+                		<div class="animation-box">
+                			<img src="static/images/animation3-1.png"  class="f-l     animation1-img" />
+                			<img src="static/images/animation3-2.png"  class="f-l img animation2-img" />
+                			<img src="static/images/animation3-3.png"  class="f-l img animation3-img" />
+                			<img src="static/images/animation3-4.png"  class="f-l img animation4-img" />
+                			<img src="static/images/animation3-5.png"  class="f-l img animation5-img" />
+                			<img src="static/images/animation3-6.png"  class="f-l img animation6-img" />
+                			<img src="static/images/animation3-7.png"  class="f-l img animation7-img" />
+                			<img src="static/images/animation3-8.png"  class="f-l img animation8-img" />
+                			<img src="static/images/animation3-9.png"  class="f-l img animation9-img" />
+                		</div>
+                	</div>     	
+                </div>	
+                <div class="c-box listab-13b">
+                	<div class="l-box">
+                		<div class="animation-box">
+                			<img src="static/images/animation4-1.png"  class="f-l     animation1-img" />
+                			<img src="static/images/animation4-2.png"  class="f-l img animation2-img" />
+                			<img src="static/images/animation4-3.png"  class="f-l img animation3-img" />
+                			<img src="static/images/animation4-4.png"  class="f-l img animation4-img" />
+                			<img src="static/images/animation4-5.png"  class="f-l img animation5-img" />
+                			<img src="static/images/animation4-6.png"  class="f-l img animation6-img" />
+                			<img src="static/images/animation4-7.png"  class="f-l img animation7-img" />
+                			<img src="static/images/animation4-8.png"  class="f-l img animation8-img" />
+                			<img src="static/images/animation4-9.png"  class="f-l img animation9-img" />
+                		</div>
+                	</div>                	
+                	<div class="r-box">
+                		<h2 class="f-l c-navy"> 个人会员申请表   </h2>
+                		<h3 class="f-l c-navy"> 会员享有下列权利：   </h3>
+                		<div class="c-box">
+                		  <h5 class="f-l c-gray">
+                        （一）本研究所的选举权、表决权和表决权。<br/>              
+                        （二）参加研究所的活动；              <br/>   
+                        （三）获得研究所服务的优先权；            <br/>     
+                        （四）对研究所工作的批评和建议权；       <br/>         
+                        （五）会员资格将是自由和自愿的。<br/>   
+                		  </h5>	
+                		  <h4 class="f-l c-navy"> 
+                		  	请下载表格并用身份证或护照寄到这个邮箱。<a href="#" class="c-navy-h c-green"> tuantihuiyuan@idc.com </a>
+                		  </h4>	
+                		</div>	
+                		<a href="#" class="btn-2 bgc-green c-white bgc-blue1-h btn-download"> 申请表下载  </a>
+                	</div>    	
+                </div>	         
+          		</ul>          		
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                loading: true
+            };
+        },
+        mounted () {
+        },
+
+        watch: { },
+        methods: {
+        },
+        components: { }
+    };
+</script>
+
+<style scoped>
---
+++ src/components/about/template/zuzhijiagou.vue
@@ -0,0 +1,26 @@
+<template>
+<ul class="f-l listab-10"><!-- Organizational Structure  -->
+                <div class="c-box">
+                  <img src="static/images/pic/zht10-1.png" class="img" />
+                </div>
+          		</ul>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                loading: true
+            };
+        },
+        mounted () {
+        },
+
+        watch: { },
+        methods: {
+        },
+        components: { }
+    };
+</script>
+
+<style scoped>
